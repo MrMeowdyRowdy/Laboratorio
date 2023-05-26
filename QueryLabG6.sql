f78919ac3a2d42992e4a554be8385ea6e8717e07
Dennis Ocaña <dennisocana@gmail.com>
2023-05-25 21:01:12 -0500

    Update QueryLabG6.sql

--- QueryLabG6.sql
+++ QueryLabG6.sql
@@ -6,14 +6,42 @@ CREATE PROCEDURE AddResultado
 AS
 IF (@FechaPedido NOT LIKE '__/__/____')
 BEGIN
-	RAISERROR ('La fecha no coincide con el formato mm/dd/aaaa')
+	RAISERROR ('La fecha no coincide con el formato mm/dd/aaaa',16,10)
 END
 ELSE
 BEGIN
-	DECLARE @EXID INT
-	SET @EXID = (SELECT idExamen FROM Examen WHERE nombre LIKE @Nexamen)
-	DECLARE @USID INT
-	SET @USID = (SELECT idUsuario FROM Paciente WHERE cedula LIKE @CI)
+	IF (SELECT idExamen FROM Examen WHERE nombre LIKE @Nexamen) IS NULL
+	BEGIN
+		RAISERROR ('El examen especificado no existe',16,10)
+	END
+	ELSE
+	BEGIN
+		DECLARE @EXID INT
+		SET @EXID = (SELECT idExamen FROM Examen WHERE nombre LIKE @Nexamen)
+	END
+	IF (SELECT idUsuario FROM Paciente WHERE cedula LIKE @CI) IS NULL
+	BEGIN
+		RAISERROR ('El paciente especificado no existe',16,10)
+	END
+	ELSE
+	BEGIN
+		DECLARE @USID INT
+		SET	@USID = (SELECT idUsuario FROM Paciente WHERE cedula LIKE @CI)
+	END
+	
 	INSERT INTO Resultado (idExamen,idUsuario,fechaPedido)
 	VALUES (@EXID,@USID,@FechaPedido)
 END
+
+GO
+
+CREATE PROCEDURE ingresoResultadoDetalles
+	--Se declaran los argumentos que recibe el procedimiento almacenado
+    @fechaExamen DATETIME,
+    @fechaEntrega DATETIME,
+    @resultado DECIMAL(7, 3)
+AS
+	--Se realiza la inserción de la tupla en la tabla Resultado.
+	INSERT INTO Resultado(fechaExamen, fechaEntrega, resultado) 
+	VALUES(@fechaExamen,@fechaEntrega,@resultado)
+GO
