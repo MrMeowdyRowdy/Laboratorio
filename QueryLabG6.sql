a39441bbf4a147bab45c0f9512d7195908585960
Dennis Ocaña <dennisocana@gmail.com>
2023-05-25 20:46:45 -0500

    Update QueryLabG6.sql

--- QueryLabG6.sql
+++ QueryLabG6.sql
@@ -4,9 +4,16 @@ CREATE PROCEDURE AddResultado
 @CI CEDULAIDENTIDAD,
 @FechaPedido DATE
 AS
-DECLARE @EXID INT
-SET @EXID = (SELECT idExamen FROM Examen WHERE nombre LIKE @Nexamen)
-DECLARE @USID INT
-SET @USID = (SELECT idUsuario FROM Paciente WHERE cedula LIKE @CI)
-INSERT INTO Resultado (idExamen,idUsuario,fechaPedido)
-VALUES (
+IF (@FechaPedido NOT LIKE '__/__/____')
+BEGIN
+	RAISERROR ('La fecha no coincide con el formato mm/dd/aaaa')
+END
+ELSE
+BEGIN
+	DECLARE @EXID INT
+	SET @EXID = (SELECT idExamen FROM Examen WHERE nombre LIKE @Nexamen)
+	DECLARE @USID INT
+	SET @USID = (SELECT idUsuario FROM Paciente WHERE cedula LIKE @CI)
+	INSERT INTO Resultado (idExamen,idUsuario,fechaPedido)
+	VALUES (@EXID,@USID,@FechaPedido)
+END
